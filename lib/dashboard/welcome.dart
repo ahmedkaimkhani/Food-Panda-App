7c584b9d75b1d5be732a782bc3fee9d88986b056
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-11 05:40:40 +0500

    Food Panda

--- lib/dashboard/welcome.dart
+++ lib/dashboard/welcome.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_panda_app/constats/color.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) :super(key: key);
@@ -12,17 +14,35 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height * 1;
     final width = MediaQuery.of(context).size.width * 1;
-    return Scaffold(
-      appBar: AppBar(
-        actions: const [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(Icons.shopping_bag_outlined),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: DrawerHeader(child: Text('Login')),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: MyColors.navy,
+          bottom: PreferredSize(
+            child: Row(
+              children: [
+                CupertinoTextField(
+                  
+                )
+              ],
+            ), 
+            preferredSize: Size.fromHeight(40),
+            ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(Icons.shopping_bag_outlined),
+            ),
+          ],
+        ),
+        drawer: const Drawer(
+          child: DrawerHeader(
+            child: Text('Login'),
+            decoration: BoxDecoration(
+              color: MyColors.navy
+            ),
+            ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:food_panda_app/splash%20screen/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
