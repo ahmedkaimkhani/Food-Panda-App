49314b1921b98a1a9af50e3f17f9c665cc0e130b
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-11 06:11:56 +0500

    FoodPanda

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      theme: ThemeData(primarySwatch:MyColors.navy ),
       home:SplashScreen(),
     );
   }
