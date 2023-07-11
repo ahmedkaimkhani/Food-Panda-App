e1888b69f5e4258aa1b91cba542d2a4458d23541
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-11 05:50:10 +0500

    Food Panda

--- lib/dashboard/welcome.dart
+++ lib/dashboard/welcome.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_panda_app/constats/color.dart';
 
@@ -14,35 +13,23 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height * 1;
     final width = MediaQuery.of(context).size.width * 1;
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: MyColors.navy,
-          bottom: PreferredSize(
-            child: Row(
-              children: [
-                CupertinoTextField(
-                  
-                )
-              ],
-            ), 
-            preferredSize: Size.fromHeight(40),
-            ),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Icon(Icons.shopping_bag_outlined),
-            ),
-          ],
-        ),
-        drawer: const Drawer(
-          child: DrawerHeader(
-            child: Text('Login'),
-            decoration: BoxDecoration(
-              color: MyColors.navy
-            ),
-            ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyColors.navy,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Icon(Icons.shopping_bag_outlined),
+          ),
+        ],
+      ),
+      drawer: const Drawer(
+        child: DrawerHeader(
+          child: Text('Login'),
+          decoration: BoxDecoration(
+            color: MyColors.navy
+          ),
+          ),
       ),
     );
   }
