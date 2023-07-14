109ef5a0eb0a745a4727c7bd77c642b79f40376b
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-14 17:05:18 +0500

    foodpanda

--- lib/dashboard/welcome.dart
+++ lib/dashboard/welcome.dart
@@ -39,7 +39,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               ),
               child: CircleAvatar(
                 radius: 24,
-                backgroundImage: AssetImage(''),
+                backgroundImage: AssetImage('images/ahmed.jpg'),
                  //backgroundColor: Colors.white,
               ),
             ),
