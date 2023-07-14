03055ad68aad251ed797dccc1d4ed0651dbadda4
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-14 20:05:51 +0500

    foodpanda

--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,38 @@ import 'package:food_panda_app/splash%20screen/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch:MyColors.navy, 
-        brightness: Brightness.light,
-        scaffoldBackgroundColor: const Color(0xfffcfcfc),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 2,
-          titleTextStyle: TextStyle(fontFamily: bold, color: Color(0xfffcfcfc)),
-          //iconTheme: IconThemeData(color: MyColors.primaryColor)
-        )
-        ),
-        darkTheme: ThemeData(
-          primarySwatch:MyColors.navy, 
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xfffcfcfc),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 2,
-          titleTextStyle: TextStyle(fontFamily: bold, color: Color(0xfffcfcfc)),
-          //iconTheme: IconThemeData(color: MyColors.primaryColor)
-        )
-        ),
-      home:SplashScreen(),
+          primarySwatch: MyColors.navy,
+          brightness: Brightness.light,
+          scaffoldBackgroundColor: const Color(0xfffcfcfc),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 2,
+            titleTextStyle:
+                TextStyle(fontFamily: bold, color: Color(0xfffcfcfc)),
+            //iconTheme: IconThemeData(color: MyColors.primaryColor)
+          )),
+      darkTheme: ThemeData(
+          primarySwatch: MyColors.navy,
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xfffcfcfc),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 2,
+            titleTextStyle:
+                TextStyle(fontFamily: bold, color: Color(0xfffcfcfc)),
+            //iconTheme: IconThemeData(color: MyColors.primaryColor)
+          )),
+      home: SplashScreen(),
     );
   }
 }
